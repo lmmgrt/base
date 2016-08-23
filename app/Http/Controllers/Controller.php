334969b9e2ba74a063f6b62efbee8d250c6c28b4
zaknight <letrunghoa90@gmail.com>
2016-08-23 09:20:32 +0700

    ok za

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,4 +11,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+    public function conGa(){
+    	return "Con vịt";
+    }
 }
